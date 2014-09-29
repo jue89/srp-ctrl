7beaa28319b84da85736223d1f6b950d390a1cb1
Jue <me@jue.yt>
2014-09-30 00:55:21 +0200

    Auth controller: Added support for flags

--- controller/auth.js
+++ controller/auth.js
@@ -8,14 +8,21 @@ module.exports = function( express ) {
     // Has been evaluated in the past
     if( this.auth ) return callback( this.auth );
 
+    // Default: Not authenticated
+    this.auth = {
+      id: null,
+      roles: { admin: false, guest: false, operator: false },
+      flags: { confirmed: false, enabled: false }
+    };
+
     // No auth header given
-    if( ! this.headers.authorization ) return callback( null );
+    if( ! this.headers.authorization ) return callback( this.auth );
 
     // PROCESS AUTHORISATION
     var authHeader = this.headers.authorization.split(' ');
 
     // Just allow BASIC auth
-    if( authHeader[ 0 ] != "Basic" ) return callback( null );
+    if( authHeader[ 0 ] != "Basic" ) return callback( this.auth );
 
     // Get credentials
     var cred = new Buffer( authHeader[ 1 ], 'base64' ).toString();
@@ -25,54 +32,71 @@ module.exports = function( express ) {
 
     // Find user
     var self = this;
-    users.get( user, { fields: [ 'password', 'roles' ] }, function( err, user ) {
-      // User not found
-      if( err ) return callback( null );
+    users.get(
+      user,
+      { fields: [ 'password', 'roles', 'confirmed', 'enabled' ] },
+      function( err, user ) {
+        // User not found or password wrong
+        if(
+          err ||
+          ! libPassword.check( password, user.password )
+        ) return callback( self.auth );
 
-      // Password wrong
-      if( ! libPassword.check( password, user.password ) ) return callback( null );
+        // Save user into request
+        self.auth = {
+          id: user.id,
+          roles: user.roles,
+          flags: { confirmed: user.confirmed, enabled: user.enabled }
+        }
 
-      // Save user into request
-      self.auth = {
-        id: user.id,
-        roles: user.roles
+        // Callback
+        callback( self.auth );
       }
-
-      // Callback
-      callback( self.auth );
-    } )
+    );
 
   }
 
   // Requires at least one of the given roles. Otherwise request from user.
-  express.request.requireAuth = function( roles, callback ) {
-    // When roles is a string -> convert to array
+  express.request.requireAuth = function( roles, flags, callback ) {
+    // When roles / flags is a string -> convert to array
     if( typeof roles == "string" ) roles = [ roles ];
+    if( typeof flags == "string" ) flags = [ flags ];
+
 
     // Anonymous is okay
-    if( roles.length == 0 ) return callback();
+    if( roles.length == 0 && flags.length == 0 ) return callback();
 
     var self = this;
     this.getAuth( function( auth ) {
       var authorised = false;
 
-      if( auth ) {
+      // User is logged on
+      if( auth.id ) {
         // Check if one required role is given
         roles.forEach( function( item ) {
-          // Required rolet given --> Authorised!
+          // Required role given --> Authorised!
           if( auth.roles[ item ] ) authorised = true;
+        } );
+
+        // Check if all required flags are given
+        flags.forEach( function( item ) {
+          // Required flag not given --> Not authorised!
+          if( ! auth.flags[ item ] ) authorised = false;
         } );
       }
 
       // Send back authorisation request, when not authorised. Otherwise: Callback
-      if( ! authorised ) {
-        self.res.statusCode = 401;
-        self.res.setHeader('WWW-Authenticate', 'Basic realm="Authorisation Required"');
-        self.res.end('Unauthorized');
-      } else {
-        callback();
-      }
+      if( ! authorised ) return self.res.endAuth();
+
+      callback();
     } );
+  }
+
+  // Helper to send authentication request
+  express.response.endAuth = function() {
+    this.statusCode = 401;
+    this.setHeader('WWW-Authenticate', 'Basic realm="Authorisation Required"');
+    this.end('Unauthorized');
   }
 
 }
