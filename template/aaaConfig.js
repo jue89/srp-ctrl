308b8dcf3a43c6f6862478a634e5f6ba6a8b7ab1
Jue <me@jue.yt>
2014-10-13 10:21:45 +0200

    Config template: IPv6 net

--- template/aaaConfig.js
+++ template/aaaConfig.js
@@ -2,7 +2,7 @@ var config = require( '../lib/config.js' );
 
 module.exports = function( aaa ) {
   return {
-    "ipv6_net": config.aaaNet,
+    "ipv6_net": config.aaaNet + ":",
     "ipv6_addr": config.aaaNet + aaa.ipv6_id,
     "mqtt": config.mqtt
   }
